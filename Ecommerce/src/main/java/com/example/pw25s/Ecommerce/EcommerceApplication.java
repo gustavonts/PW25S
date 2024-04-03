0cb313ee3e2d6c5614c388ce459383f35ce6ba4f
Vitinho_png <marcantevitor@outlook.com>
2024-04-03 10:38:26 -0300

    Initial commit

---
+++ Ecommerce/src/main/java/com/example/pw25s/Ecommerce/EcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.pw25s.Ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceApplication.class, args);
+	}
+
+}
